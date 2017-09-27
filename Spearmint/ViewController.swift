231c38f9d763588c9a813f65bacf06e23cef9819
Sebastian Shanus <sedward@berkeley.edu>
2017-09-26 20:42:15 -0700

    Initial Commit

---
+++ Spearmint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spearmint
+//
+//  Created by Sebastian Shanus on 9/26/17.
+//  Copyright © 2017 Sebastian Shanus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
